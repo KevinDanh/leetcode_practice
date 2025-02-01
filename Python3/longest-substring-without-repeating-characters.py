4bba8069266cdb30ccab851087e41180d0921554
Kevin Danh <kevindanh21@gmail.com>
2025-02-01 13:59:48 -0800

    Add: Leetcode #3 Longest Substring without repeating characters

---
+++ Python3/longest-substring-without-repeating-characters.py
@@ -0,0 +1,16 @@
+def solution(self, s: str) -> int:
+    int count
+    int temp
+    int pointer
+
+    while pointer != len(s):
+        break
+    return 0
+
+def main() -> None:
+    result = solution()
+    print("Result: " + str(result))
+    
+if __name__ == '__main__':
+    main()
+    
