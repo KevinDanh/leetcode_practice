f0380ed95b1b68ca1f62e51ae3f47435947c6c4f
dev_kdanh <kevindanh21@gmail.com>
2025-02-23 20:55:34 -0800

    Added Sollution for 226. Invert Binary Tree

---
+++ Python3/invert-binary-tree.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def invertTree(self, root):
+        if not root:
+            return None
+
+        stack = [root]
+
+        while stack:
+            current = stack.pop()
+            current.left, current.right = current.right, current.left
+            if current.left:
+                stack.append(current.left)
+            if current.right:
+                stack.append(current.right)
+
+        return root
